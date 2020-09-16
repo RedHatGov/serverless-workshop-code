6ccf3e582c25e4315e0d747ce2948f065c75fc14
theckang <chriskang@gmail.com>
2020-09-16 15:39:47 -0500

    Load S3 model once instead of on every request

--- model/prediction/prediction.py
+++ model/prediction/prediction.py
@@ -1,9 +1,17 @@
 import pickle, boto3
 from flask import Flask, request, abort, jsonify
+from boto3.s3.transfer import TransferConfig
 
+application = Flask(__name__)
+
+# Load the vectorizer and model from S3
 bucket_name = 'serverless-workshop-model'
 model_file_name = 'model.pkl'
-application = Flask(__name__)
+
+s3 = boto3.resource('s3')
+s3.Bucket(bucket_name).download_file(model_file_name, model_file_name, Config=TransferConfig(use_threads=False))
+with open(model_file_name, 'rb') as f:
+    cv, clf = pickle.load(f)
 
 @application.route('/predict', methods=['POST'])
 def predict():
@@ -13,12 +21,6 @@ def predict():
         if not request.json or not 'text' in request.json:
             abort(400)
         text = request.json['text']
-
-        # Load the vectorizer and model from S3
-        s3 = boto3.resource('s3')
-        s3.Bucket(bucket_name).download_file(model_file_name, model_file_name)
-        with open(model_file_name, 'rb') as f:
-            cv, clf = pickle.load(f)
 
         # Return prediction
         prediction = clf.predict(cv.transform([text]))[0]      # Do not use .fit_transform() here
