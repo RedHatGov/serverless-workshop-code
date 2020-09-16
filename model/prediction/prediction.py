a73bd250a29a02cab45b94628b64b04f2601a8d1
theckang <chriskang@gmail.com>
2020-09-16 15:45:11 -0500

    Clean up prediction app code

--- model/prediction/prediction.py
+++ model/prediction/prediction.py
@@ -5,6 +5,7 @@ from boto3.s3.transfer import TransferConfig
 application = Flask(__name__)
 
 # Load the vectorizer and model from S3
+# Disable S3 multi threading to prevent gunicorn worker deadlock
 bucket_name = 'serverless-workshop-model'
 model_file_name = 'model.pkl'
 
@@ -15,13 +16,12 @@ with open(model_file_name, 'rb') as f:
 
 @application.route('/predict', methods=['POST'])
 def predict():
+    """Returns a disaster prediction by running the request text through NLP model"""
     if request.method == 'POST':
 
-        # Load the request text
         if not request.json or not 'text' in request.json:
             abort(400)
         text = request.json['text']
 
-        # Return prediction
         prediction = clf.predict(cv.transform([text]))[0]      # Do not use .fit_transform() here
         return jsonify({"disaster": True if prediction == 1 else False})
