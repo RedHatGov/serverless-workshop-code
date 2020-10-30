fe59180598a1a27494b44f313a273fde0585f234
theckang <chriskang@gmail.com>
2020-10-30 14:54:09 -0500

    Add endpoint url for OCS

--- model/prediction/prediction.py
+++ model/prediction/prediction.py
@@ -5,18 +5,21 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 application = Flask(__name__)
 
-# Load the vectorizer and model from S3
-# Disable S3 multi threading to prevent gunicorn worker deadlock
+# Load env vars
+endpoint_url=os.getenv("ENDPOINT_URL")
 bucket_name=os.getenv("BUCKET_NAME")
 model_file_name=os.getenv("MODEL_FILE_NAME")
+lifeline_url = os.getenv("LIFELINE_URL")
 
-s3 = boto3.resource('s3')
+# Load the vectorizer and model from S3
+# Disable S3 multi threading to prevent gunicorn worker deadlock
+if endpoint_url:
+    s3 = boto3.resource('s3', endpoint_url=endpoint_url)
+else:
+    s3 = boto3.resource('s3')
 s3.Bucket(bucket_name).download_file(model_file_name, model_file_name, Config=TransferConfig(use_threads=False))
 with open(model_file_name, 'rb') as f:
     cv, clf = pickle.load(f)
-
-# Set lifeline route
-lifeline_url = os.getenv("LIFELINE_URL")
 
 @application.route('/predict', methods=['POST'])
 def predict():
